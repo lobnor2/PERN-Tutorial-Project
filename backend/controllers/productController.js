48b9c4f83238458d49e9793c3e4ca1dc19b2fae8
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-16 00:49:16 -0500

    add getproducts function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,25 @@
-export const getAllProducts = async (req, res) => {};
+import { sql } from "../config/db.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await sql`SELECT * FROM products
+    ORDER BY created_at DESC`;
+    console.log("fetched products", products);
+    res.status(200).json({
+      success: true,
+      count: products.length,
+      data: products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Server error",
+    });
+  }
+};
 
 export const createProduct = async (req, res) => {};
+
+export const getProduct = async (req, res) => {};
+export const updateProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,18 @@
 import express from "express";
 import {
   createProduct,
-  getAllProducts,
+  deleteProduct,
+  getProducts,
+  getProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get("/", getAllProducts);
-
+router.get("/", getProducts);
+router.get("/:id", getProduct);
 router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
