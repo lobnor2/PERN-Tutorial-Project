787a6354ea803f209b4a13c170855552db3639f7
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-15 21:01:53 -0500

    add routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const PORT = 3000;
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/test", (req, res) => {
-  console.log(res.getHeaders());
-  res.send("Hello from test route");
-});
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+//ALL ROUTES
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
