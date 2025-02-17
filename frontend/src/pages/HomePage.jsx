a610389ac5424819a1c0e04b0a01110401a8c4c0
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-17 15:23:19 -0500

    add refresh button, add new product button and all the products

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { useProductStore } from "@/store/useProductStore";
+import React, { useEffect } from "react";
+import { RefreshCcw } from "lucide-react";
+import ProductCard from "@/components/ProductCard";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { products, loading, error, fetchProducts } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  console.log("this is products", products);
+  return (
+    <div>
+      <div className="flex items-center justify-between border border-blue-600 px-16 mt-5">
+        <Button className="cursor-pointer">Add Product</Button>
+        <Button
+          className="w-16 cursor-pointer"
+          variant="outline"
+          onClick={fetchProducts}
+        >
+          <RefreshCcw className="" />
+        </Button>
+      </div>
+      {error && <div className="text-red-500">{error}</div>}
+      {loading ? (
+        <div className="flex items-center justify-center text-3xl">
+          Loading...
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-5 border border-green-700 p-5">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
