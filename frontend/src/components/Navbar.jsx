f0b6ebf392bb131c09a1ed2ad5c8cec79d5a06fb
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-17 15:21:36 -0500

    add logo and cart on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+  return (
+    <div className="border border-red-700 flex justify-between p-5">
+      <div className="">
+        <Link to="/">LOGO</Link>
+      </div>
+      {isHomePage && <div>Cart</div>}
+    </div>
+  );
 };
 
 export default Navbar;
