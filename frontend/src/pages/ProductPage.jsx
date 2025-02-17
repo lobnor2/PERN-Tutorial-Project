4be37e680eb2ed3ca6e6ebf8d2aa591a8bd049ce
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-17 15:22:41 -0500

    add product page

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductPage = () => {
-  return <div>ProductPage</div>;
+  const { id } = useParams();
+  return <div>ProductPage {id}</div>;
 };
 
 export default ProductPage;
