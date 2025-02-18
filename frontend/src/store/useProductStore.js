d225689b47f07cbb31a9348b3b94eecb64260fcb
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-17 22:51:34 -0500

    add delete product function

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const BASE_URL = "http://localhost:3000";
 const api = axios.create({
@@ -18,8 +19,25 @@ export const useProductStore = create((set, get) => ({
       const res = await api.get("/api/products");
       set({ products: res.data.data, error: null });
     } catch (error) {
-      if (error.status === 429) set({ error: "Rate limit exceeded" });
-      else set({ error: "Something went wrong" });
+      if (error.status === 429)
+        set({ error: "Rate limit exceeded", products: [] });
+      else set({ error: "Something went wrong", products: [] });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  deleteProduct: async (id) => {
+    try {
+      set({ loading: true });
+      await api.delete(`/api/products/${id}`);
+      set((prev) => ({
+        products: prev.products.filter((product) => product.id !== id),
+      }));
+      toast.success("Product deleted successfully");
+    } catch (error) {
+      toast.error("Something went wrong");
+      //   console.log(error);
     } finally {
       set({ loading: false });
     }
