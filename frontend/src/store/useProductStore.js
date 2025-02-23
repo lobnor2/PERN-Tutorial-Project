69b7a49b67395e34bcdc16b2a280d8955ccb914b
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-22 23:51:51 -0500

    add updateproduct function and fetch product for edit

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -12,6 +12,7 @@ export const useProductStore = create((set, get) => ({
   products: [],
   loading: false,
   error: null,
+  currentProduct: null,
   isOpen: false,
   setIsOpen: (open) => set({ isOpen: open }),
 
@@ -84,6 +85,36 @@ export const useProductStore = create((set, get) => ({
     } catch (error) {
       toast.error("Something went wrong");
       //   console.log(error);
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  fetchProduct: async (id) => {
+    set({ loading: true });
+    try {
+      const response = await api.get(`/api/products/${id}`);
+      set({
+        currentProduct: response.data.data,
+        formData: response.data.data, //pre-fill form with current product data
+        error: null,
+      });
+    } catch (error) {
+      set({ error: "Something went wrong", currentProduct: null });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  updateProduct: async (id) => {
+    set({ loading: true });
+    try {
+      const { formData } = get();
+      await api.put(`/api/products/${id}`, formData);
+      await get().fetchProducts();
+      toast.success("Product updated successfully");
+    } catch (error) {
+      toast.error("Something went wrong");
     } finally {
       set({ loading: false });
     }
